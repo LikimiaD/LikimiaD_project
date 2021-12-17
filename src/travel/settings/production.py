03088b4e9f9b35c98984aec383ba9a9ed702b4d0
LikimiaD <somethingforsoftware@gmail.com>
2021-12-17 19:42:25 +0300

    main

--- src/travel/settings/production.py
+++ src/travel/settings/production.py
@@ -132,6 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
